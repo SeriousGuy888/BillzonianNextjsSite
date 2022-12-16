9af3695fb7d9446bc7540a5611ef5fc03eb1e2fb
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2022-12-16 01:49:24 +0000

    Deploying to gh-pages from @ SeriousGuy888/BillzonianNextjsSite@cc5a72474024d7b3b7175dd646c2586652299e40 🚀

---
+++ _next/static/XxsaVose6GE0icisYYQga/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/19c23e8b49e0172b.css","static/chunks/pages/index-87bcf4b096b28e30.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
