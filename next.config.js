1e10e55393fe4e5ecd3431b59f667734c2345cd0
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2022-12-15 21:07:38 -0500

    fix assetprefix with trailing slash

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
+
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH ?? ""
+const assetPrefix = basePath ? basePath + "/" : ""
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  basePath,
+  assetPrefix,
 }
 
 module.exports = nextConfig
