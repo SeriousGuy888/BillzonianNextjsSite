0f23e360cd886ebd3f6b3e683bbd91db5b0eb9fb
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2022-12-16 00:21:09 -0500

    comment out stuff for deploying on github pages

--- next.config.js
+++ next.config.js
@@ -1,16 +1,16 @@
 const path = require("path")
 
-const basePath = process.env.NEXT_PUBLIC_BASE_PATH ?? ""
-const assetPrefix = basePath ? basePath + "/" : ""
+// const basePath = process.env.NEXT_PUBLIC_BASE_PATH ?? ""
+// const assetPrefix = basePath ? basePath + "/" : ""
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    unoptimized: true,
-  },
-  basePath,
-  assetPrefix,
+  // images: {
+  //   unoptimized: true,
+  // },
+  // basePath,
+  // assetPrefix,
   swcMinify: true,
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
