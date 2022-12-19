8e2921232d140948f90036970bec5802b694f94b
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2022-12-19 16:22:52 -0500

    header and nav layout

---
+++ components/Nav.tsx
@@ -0,0 +1,3 @@
+export default function Nav() {
+  return <nav className="layout-nav">Sidenav</nav>
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
 import "../styles/globals.scss"
+import "../styles/layout.scss"
+
 import type { AppProps } from "next/app"
 
 import { Montserrat } from "@next/font/google"
+import Header from "../components/Header"
+import Nav from "../components/Nav"
+
 const montserrat = Montserrat({ subsets: ["latin"] })
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -13,7 +18,13 @@ export default function App({ Component, pageProps }: AppProps) {
         }
       `}</style>
 
-      <Component {...pageProps} />
+      <section className="layout">
+        <Header />
+        <Nav />
+        <div className="content-section">
+          <Component {...pageProps} />
+        </div>
+      </section>
     </>
   )
 }
