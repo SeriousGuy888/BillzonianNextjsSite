763d2340069675e8ebe5909469ebffb50bb32711
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2022-12-19 17:42:32 -0500

    added font styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,24 +7,20 @@ import { Montserrat } from "@next/font/google"
 import Header from "../components/Header"
 import Nav from "../components/Nav"
 
-const montserrat = Montserrat({ subsets: ["latin"] })
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  weight: ["300", "400", "700"],
+  variable: "--montserrat",
+})
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <style jsx global>{`
-        html {
-          font-family: ${montserrat.style.fontFamily};
-        }
-      `}</style>
-
-      <section className="layout">
-        <Header />
-        <Nav />
-        <div className="content-section">
-          <Component {...pageProps} />
-        </div>
-      </section>
-    </>
+    <main className={`${montserrat.variable} layout`}>
+      <Header />
+      <Nav />
+      <div className="contentSection">
+        <Component {...pageProps} />
+      </div>
+    </main>
   )
 }
