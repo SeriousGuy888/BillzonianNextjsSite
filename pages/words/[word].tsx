9d373a60adec56cce6eabb1660fd39d778949560
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2022-12-20 01:21:25 -0500

    bare bones word definition page

--- pages/words/[word].tsx
+++ pages/words/[word].tsx
@@ -9,53 +9,48 @@ const notoSans = Noto_Sans({ weight: "400" })
 export default function Word() {
   const router = useRouter()
   const { word } = router.query
-  const entry = getWord(word?.toString() ?? "")
+  const entries = getWord(word?.toString() ?? "")
 
-  if (entry instanceof Error) {
-    return <main>Word not found.</main>
+  if (entries instanceof Error) {
+    return <p>Word not found.</p>
   }
 
   return (
-    <main className={styles.main}>
-      <section className={styles.entryWord}>
-        <h1>{entry.word}</h1>
-        <p className={notoSans.className}>
-          {entry.pronunciations.map((pronunciation, index) => (
-            <span className={styles.ipaReading} key={index}>
-              /{pronunciation}/
-            </span>
-          ))}
-        </p>
-        <br />
-        <p>Also spelled
-          {entry.alternateForms.map((alt, index) => (
-            <Link href={`/words/${alt}`} key={index}>
-              {alt}
-            </Link>
-          ))}
-        </p>
-      </section>
-      <section>
-        <ol>
-          {entry.glosses.map((trans, index) => (
-            <li key={index}>{trans}</li>
-          ))}
-        </ol>
-      </section>
-      <section>
-        <ol className={styles.letterList}>
-          {entry.examples.map((examp, index) => (
-            <li key={index}>{examp}</li>
-          ))}
-        </ol>
-      </section>
-      <section>
-        <ul>
-          {entry.notes.map((note, index) => (
-            <li key={index}>{note}</li>
-          ))}
-        </ul>
-      </section>
-    </main>
+    <section className={styles.main}>
+      <>
+        <h1>{word}</h1>
+        {entries.map((entry, i) => {
+          return (
+            <article key={i}>
+              <p>Entry {i + 1}</p>
+              <h3>{entry.partOfSpeech}</h3>
+              <ol>
+                {entry.glosses.map((gloss, j) => (
+                  <li key={j}>{gloss}</li>
+                ))}
+              </ol>
+              <ol>
+                {entry.examples.map((example, j) => (
+                  <li key={j}>{example}</li>
+                ))}
+              </ol>
+              <ul>
+                {entry.notes.map((note, j) => (
+                  <li key={j}>{note}</li>
+                ))}
+              </ul>
+              <p>
+                Also spelled as{" "}
+                {entry.alternateForms.map((altForm, j) => (
+                  <Link key={j} href={`/words/${altForm}`}>
+                    <span>{altForm}</span>
+                  </Link>
+                ))}
+              </p>
+            </article>
+          )
+        })}
+      </>
+    </section>
   )
 }
