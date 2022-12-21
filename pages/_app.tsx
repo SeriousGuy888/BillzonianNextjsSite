957f39e983bf7f7fbb166c3ba7d241eb12027d33
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2022-12-21 12:20:33 -0500

    use regular css import instead of @next/fonts
    nextjs fonts werent working D:

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,18 +3,10 @@ import "../styles/layout.scss"
 
 import type { AppProps } from "next/app"
 
-import { Montserrat } from "@next/font/google"
 import Nav from "../components/elements/nav/Nav"
-
-const montserrat = Montserrat({
-  subsets: ["latin"],
-  weight: ["300", "400", "700"],
-  variable: "--montserrat",
-})
-
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className={`${montserrat.variable} layout`}>
+    <div className="layout">
       <Nav />
       <main>
         <Component {...pageProps} />
