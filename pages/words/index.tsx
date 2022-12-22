6f1ef6334f540fe9b15dfbac6ad330a52d275a08
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2022-12-22 18:58:40 -0500

    title on dictionary browse page

--- pages/words/index.tsx
+++ pages/words/index.tsx
@@ -2,18 +2,24 @@ import { getWordsOnPage } from "../../utils/dictionaryData"
 import WordLink from "../../components/elements/wordlist/WordLink"
 import styles from "../../styles/Wordlist.module.scss"
 import { motion } from "framer-motion"
+import Head from "next/head"
 
 export default function Words({ words }: { words: string[] }) {
   return (
-    <motion.section
-      className={styles.wordList}
-      initial={{ y: 25 }}
-      animate={{ y: 0 }}
-    >
-      {words.map((w) => (
-        <WordLink key={w} word={w} />
-      ))}
-    </motion.section>
+    <>
+      <Head>
+        <title>Billzonian Dictionary</title>
+      </Head>
+      <motion.section
+        className={styles.wordList}
+        initial={{ y: 25 }}
+        animate={{ y: 0 }}
+      >
+        {words.map((w) => (
+          <WordLink key={w} word={w} />
+        ))}
+      </motion.section>
+    </>
   )
 }
 
