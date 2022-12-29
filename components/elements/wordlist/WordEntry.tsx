e3f83117fbc27a63bf0075351b1cc632b54bae1b
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2022-12-29 16:18:08 -0500

    extract word entries into component

---
+++ components/elements/wordlist/WordEntry.tsx
@@ -0,0 +1,95 @@
+import { Noto_Sans } from "@next/font/google"
+import { motion } from "framer-motion"
+import { NextPage } from "next"
+import Link from "next/link"
+import { DictionaryEntry } from "../../../types/DictionaryTypes"
+import { cachedWordData } from "../../../utils/dictionaryData"
+import styles from "../../../styles/Word.module.scss"
+
+const notoSans = Noto_Sans({
+  weight: "400",
+  subsets: ["latin", "latin-ext"],
+})
+
+interface PageProps {
+  entry: DictionaryEntry
+  entryIndex: number
+}
+
+const WordEntry: NextPage<PageProps> = ({ entry, entryIndex }) => {
+  return (
+    <motion.article
+      key={entry.glosses[0]}
+      className={styles.entry}
+      initial={{ y: 50 }}
+      animate={{ y: 0 }}
+      transition={{ delay: entryIndex * 0.1 }}
+    >
+      <p className={styles.entryNumber}>Entry {entryIndex + 1}</p>
+
+      <h3>{entry.partOfSpeech}</h3>
+
+      <hr />
+
+      {entry.glosses.length > 0 && (
+        <ol>
+          {entry.glosses.map((gloss) => (
+            <li key={gloss}>{gloss}</li>
+          ))}
+        </ol>
+      )}
+      {entry.examples.length > 0 && (
+        <ol className={styles.letterList}>
+          {entry.examples.map((example) => (
+            <li key={example}>{example}</li>
+          ))}
+        </ol>
+      )}
+      {entry.notes.length > 0 && (
+        <ul>
+          {entry.notes.map((note) => (
+            <li key={note}>{note}</li>
+          ))}
+        </ul>
+      )}
+
+      <hr />
+
+      <p className={styles.pronunciationList}>
+        {entry.pronunciations.map((pronunciation) => (
+          <span
+            key={pronunciation}
+            className={styles.ipaReading}
+            style={notoSans.style}
+          >
+            /{pronunciation}/
+          </span>
+        ))}
+      </p>
+      {entry.alternateForms.length > 0 && (
+        <p>
+          Alternate forms / spellings:{" "}
+          <span className={styles.altForms}>
+            {entry.alternateForms.map((altForm) => (
+              <span key={altForm}>
+                <Link
+                  href={`/words/${altForm}`}
+                  className={wordExists(altForm) ? "" : "redLink"}
+                  title={wordExists(altForm) ? "" : "This page does not exist."}
+                >
+                  {altForm}
+                </Link>
+              </span>
+            ))}
+          </span>
+        </p>
+      )}
+    </motion.article>
+  )
+}
+
+const wordExists = (word: string) => {
+  return word in cachedWordData
+}
+
+export default WordEntry
--- pages/words/[word].tsx
+++ pages/words/[word].tsx
@@ -1,21 +1,14 @@
 import {
-  cachedWordData,
   getWord,
   padWord,
   uniqueWordsList,
 } from "../../utils/dictionaryData"
 import styles from "../../styles/Word.module.scss"
 
-import { Noto_Sans } from "@next/font/google"
-import Link from "next/link"
 import Head from "next/head"
 import { GetStaticPaths, GetStaticProps, NextPage } from "next"
 import { DictionaryEntry } from "../../types/DictionaryTypes"
-const notoSans = Noto_Sans({
-  weight: "400",
-  subsets: ["latin", "latin-ext"],
-})
-import { motion } from "framer-motion"
+import WordEntry from "../../components/elements/wordlist/WordEntry"
 
 const Word: NextPage<{
   entries?: DictionaryEntry[]
@@ -52,88 +45,12 @@ const Word: NextPage<{
 
       <section className={styles.main}>
         <h1>{word}</h1>
-        {entries.map((entry, i) => {
-          return (
-            <motion.article
-              key={entry.glosses[0]}
-              className={styles.entry}
-              initial={{ y: 50 }}
-              animate={{ y: 0 }}
-              transition={{ delay: i * 0.1 }}
-            >
-              <p className={styles.entryNumber}>Entry {i + 1}</p>
-
-              <h3>{entry.partOfSpeech}</h3>
-
-              <hr />
-
-              {entry.glosses.length > 0 && (
-                <ol>
-                  {entry.glosses.map((gloss) => (
-                    <li key={gloss}>{gloss}</li>
-                  ))}
-                </ol>
-              )}
-              {entry.examples.length > 0 && (
-                <ol className={styles.letterList}>
-                  {entry.examples.map((example) => (
-                    <li key={example}>{example}</li>
-                  ))}
-                </ol>
-              )}
-              {entry.notes.length > 0 && (
-                <ul>
-                  {entry.notes.map((note) => (
-                    <li key={note}>{note}</li>
-                  ))}
-                </ul>
-              )}
-
-              <hr />
-
-              <p className={styles.pronunciationList}>
-                {entry.pronunciations.map((pronunciation) => (
-                  <span
-                    key={pronunciation}
-                    className={styles.ipaReading}
-                    style={notoSans.style}
-                  >
-                    /{pronunciation}/
-                  </span>
-                ))}
-              </p>
-              {entry.alternateForms.length > 0 && (
-                <p>
-                  Alternate forms / spellings:{" "}
-                  <span className={styles.altForms}>
-                    {entry.alternateForms.map((altForm) => (
-                      <span key={altForm}>
-                        <Link
-                          href={`/words/${altForm}`}
-                          className={wordExists(altForm) ? "" : "redLink"}
-                          title={
-                            wordExists(altForm)
-                              ? ""
-                              : "This page does not exist."
-                          }
-                        >
-                          {altForm}
-                        </Link>
-                      </span>
-                    ))}
-                  </span>
-                </p>
-              )}
-            </motion.article>
-          )
-        })}
+        {entries.map((entry, i) => (
+          <WordEntry key={entry.glosses[0]} entry={entry} entryIndex={i} />
+        ))}
       </section>
     </>
   )
-}
-
-const wordExists = (word: string) => {
-  return word in cachedWordData
 }
 
 export const getStaticProps: GetStaticProps = (context) => {
